d44053328335ef89cf1cdeb0b317ed2ddef20f74
Yao Yu <yao97ming@gmail.com>
2020-03-06 16:19:24 -0800

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Yao Yu on 3/6/20.
+//  Copyright © 2020 Yao Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
